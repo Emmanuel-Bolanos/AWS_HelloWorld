5c1e72e603f4853826bfdcfdf14083d08e3a48f0
Emmanuel-Bolanos <Emmanuel.Bolanos@theksquaregroup.com>
2021-03-24 22:34:01 -0600

    update git route in bash

--- script.sh
+++ script.sh
@@ -3,12 +3,10 @@ sudo apt-get update
 sudo apt-get install nginx git mysql-client -y
 rm -rf /tmp/website
 sleep 1
-git clone https://github.com/daronwolff/demo-html.git /tmp/website
+git clone https://github.com/Emmanuel-Bolanos/AWS_HelloWorld.git /tmp/website
 sleep 1
 sudo cp /tmp/website/* /var/www/html/
 sudo service nginx stop
 sleep 1
 sudo service nginx start
 touch /tmp/done.txt
-
-##
